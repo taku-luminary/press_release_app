0259a7df1f71ef56006c00f10e17e5b387bc7e6f
Takuma Awaki <takuma@TakumanoMacBook-Air.local>
2025-06-25 14:03:34 +0900

    LINE連携　修正2回目

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,5 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link_tree ../../../vendor/javascript .js
-//= link_tree ../../javascript .js
+// Rails 7 では JavaScript は Importmap／esbuild 側で管理するため
+// Sprockets での precompile には含めない。
+//= link_tree ../builds
